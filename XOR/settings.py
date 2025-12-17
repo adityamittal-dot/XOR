605b96437229a2a46097fdd845989b1baf8dbb8b
adityamittal-dot <adityamittal529@gmail.com>
2025-12-17 16:43:16 +0530

    creating branch Dockerize-XOR

--- XOR/settings.py
+++ XOR/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5=v$9&wq107ljp3dd%#+%kaokn)9fg-=dd=z*5_ledcowd^luc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 # Application definition
 
