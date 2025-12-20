6e2ae24b2611da3d94cdd7ab91631f395e1a0d91
adityamittal-dot <adityamittal529@gmail.com>
2025-12-20 14:46:54 +0530

    migrate base badge component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import { Button } from "./components/ui/button";
+import { Badge } from "./components/ui/badge";
 
 export default function App() {
   return (
-    <div className="p-8">
+    <div className="p-8 space-y-4">
       <Button>Click me</Button>
+      <Badge>Default</Badge>
     </div>
   );
 }
