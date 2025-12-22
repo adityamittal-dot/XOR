ac5447e9134179ec16711caeb208ee262e56d464
adityamittal-dot <adityamittal529@gmail.com>
2025-12-22 20:01:43 +0530

    resolved merge conflicts

--- home/views.py
+++ home/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+import random
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+QUOTES = [
+    "Stay hungry, stay foolish.",
+    "Code is like humor. When you have to explain it, it’s bad.",
+    "Simplicity is the soul of efficiency.",
+    "First, solve the problem. Then, write the code.",
+]
+
+@api_view(["GET"])
+def user_quote(request):
+    user = request.user
+
+    return Response({
+        "username": user.username if user.is_authenticated else "Guest",
+        "quote": random.choice(QUOTES),
+    })
