0e77f0a4f705ead65aea2f11425235cb4da3f901
adityamittal-dot <adityamittal529@gmail.com>
2026-01-22 13:24:19 +0530

    add JWT-protected notes CRUD API

--- XOR/settings.py
+++ XOR/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'home',
     'rest_framework',
     'accounts',
+    'notes',
 ]
 
 MIDDLEWARE = [
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
-from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.tokens import RefreshToken 
 from rest_framework.permissions import IsAuthenticated
 
 from .serializers import RegisterSerializer, LoginSerializer, UserMeSerializer
